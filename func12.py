8ea5e4afb8ebe8f80307b0f187f18f5abec07d22
jiwan1721 <chandjiwn17@hotmail.com>
2022-04-17 22:53:36 +0545

    practice home

--- func12.py
+++ func12.py
@@ -1,23 +1,27 @@
 
 TO_DO_lIST = []
-def create_list(list1):
-    list1 =list1.split()
-    
-    
-    TO_DO_lIST.extend(list1)
+def create_list():
+    list_input=input("enter list: ")
+    list_input=list_input.split()
+    TO_DO_lIST.extend(list_input)
     print("your list is: ",TO_DO_lIST)
-    
     return TO_DO_lIST
 
-def update_list(index_number, number):
-    #create_list(pop(index_number))
-   # upadate_index = create_list(insert())
-    TO_DO_lIST.pop(index_number)
-    TO_DO_lIST.insert(index_number,number)
-    print("updated list",TO_DO_lIST)
+def update_list():
+    try:
+        index_number = int(input("enter index number: "))
+        number = input("enter the number you want to update: ")
+        #create_list(pop(index_number))
+        # upadate_index = create_list(insert())
+        TO_DO_lIST.pop(index_number)
+        TO_DO_lIST.insert(index_number,number)
+        print("updated list",TO_DO_lIST)
+    except ValueError:
+        print("give input integer only: ")
+       
     
 def delete():
-    delete_list =0
+    delete_list =[]
     delete_list.extend(TO_DO_lIST)
     del delete_list
     print("list is deleted")
@@ -32,24 +36,20 @@ print("type '0' to create list  \ntype '1' update by index  \ntype '2' to delete
 try:
     user_input =int((input("enter number: ")))
     if 0<=user_input<=2:
-        if user_input==0:
-            list_input=input("enter list: ")
-            create_list(list_input)
+        if user_input==0:  
+            create_list()
             try:
                 user_input_operation =int((input("enter number: ")))
-                if user_input_operation==1:
-                    try:
-                        index_input = int(input("enter index number: "))
-                        number_input = input("enter the number you want to update: ")
-                        update_list(index_input,number_input)
-                    except ValueError:
-                         print("only give input of integer")
-                elif user_input==2:
+                if user_input_operation==1:      
+                    update_list()
+                elif user_input_operation==2:
                     delete()
-                elif user_input==3:
+                elif user_input_operation==3:
                     delete_index()
             except ValueError:
                 print("give integer input")
+            
+        
         else:
             print("first enter '0' please: ")
     else:
