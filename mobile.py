b08554503e3c9d1ba9dff4720120ecebaf3357ca
jiwan chanda <chandjiwn9@gmail.com>
2022-04-25 11:28:29 +0545

    from intern

--- mobile.py
+++ mobile.py
@@ -5,6 +5,7 @@ def mobile_category():
     samsung_list=[]
     apple_list = []
     Mi_list = []
+    mobile_dict ={}
   
     
     for row in file[1:]:
@@ -15,44 +16,42 @@ def mobile_category():
         description_splited = row_splited[2]
         tag_splited = row_splited[3]
         price_splited = row_splited[4]
-        if category_splited=='samsung':
-            samsung_dict={
-                    'name':name_splited,
-                    'description':description_splited,
-                    'tag':tag_splited,
-                    'price':price_splited[:-1]  
-            }
-            samsung_list.append(samsung_dict)
-        if category_splited=="apple":
-            apple_dict={
-                'name':name_splited,
-                'description':description_splited,
-                'tag':tag_splited,
-                'price':price_splited[:-1]
-            }
-            apple_list.append(apple_dict)
-        if category_splited=="MI":
-            MI_dict={
-                    'name':name_splited,
-                    'description':description_splited,
-                    'tag':tag_splited,
-                    'price':price_splited[:-1]
-            }     
-            Mi_list.append(MI_dict)
-        mobile_dict = {
-            'category':{
-                'apple':apple_list,
-                'samsung':samsung_list,
-                'Mi':Mi_list
-            }
-        }
-    mobile_detail.append(mobile_dict)
-    return mobile_detail
-
+        
+        # if category_splited=='samsung':
+        #     samsung_dict={
+        #             'name':name_splited,
+        #             'description':description_splited,
+        #             'tag':tag_splited,
+        #             'price':price_splited[:-1]  
+        #     }
+        #     samsung_list.append(samsung_dict)
+        # if category_splited=="apple":
+        #     apple_dict={
+                
+        #         'name':name_splited,
+        #         'description':description_splited,
+        #         'tag':tag_splited,
+        #         'price':price_splited[:-1]
+        #     }
+        #     apple_list.append(apple_dict)
+        # if category_splited=="MI":
+        #     MI_dict={
+        #             'name':name_splited,
+        #             'description':description_splited,
+        #             'tag':tag_splited,
+        #             'price':price_splited[:-1]
+        #     }     
+        #     Mi_list.append(MI_dict)
+        # mobile_dict = {
+            
+        #         'apple':apple_list,
+        #         'samsung':samsung_list,
+        #         'Mi':Mi_list
+            
+        # }
+        mobile_detail.append(category_splited)
+    return set(mobile_detail)
 def search_by_nmae(name):
-   
-    
-  
     name_list = []
     for item in file[1:]:
         namelist = item.split(",")
@@ -70,9 +69,23 @@ def search_by_nmae(name):
             name_list.append(name_dict)
         else:
             print("not found")
+    return name_list
     
+def menu():
+    user_input = int(input('enter 1 to see category: \nenter 2 to search by name \nenter 3 to search \nenter 4 to filter by price\nenter here: '))
+    if user_input ==1:
+        print(mobile_category())
+    elif user_input ==2:
+        user_input2 = input("please give name of device: ")
+        search_by_nmae(user_input2)
+    elif user_input ==3:
+        user_input3 = input("type anaything you want to search: ")
+    elif user_input ==4:
+        pass
 
-print(search_by_nmae('Iphone10'))
+menu()
+
+
             
 
 
